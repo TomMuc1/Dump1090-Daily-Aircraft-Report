996c1ff0fe66686331d4222b36e93ea4d34234a2
TomMuc1 <31601862+TomMuc1@users.noreply.github.com>
2017-09-10 19:58:58 +0200

    Update ac_counter.php

--- ac_counter.php
+++ ac_counter.php
@@ -5,10 +5,10 @@
 #ini_set('error_reporting', E_ALL);
 
 // set your google email-address daily reports to send to
-$user_set_array['email_address'] = 'YOUR_EMAIL@gmail.com';
+$user_set_array['email_address'] = 'tommuccc@gmail.com';
 
 // set path to aircraft.json file
-$user_set_array['url_json'] = 'http://127.0.0.1/dump1090/data/aircraft.json';
+$user_set_array['url_json'] = 'http://192.168.143.10/dump1090/data/aircraft.json';
 
 // set the absolute limit of alert-messages default is 500 this script is allowed to send over its whole runtime
 $user_set_array['mailer_limit'] = 500;
@@ -22,9 +22,9 @@ $seconds_of_day = time() - strtotime('today');
 
 while (true) {
 
-    $start_loop_microtime = microtime(true);
-    if ($seconds_of_day > time() - strtotime('today')) {
-        if ($sent_messages < $user_set_array['mailer_limit']) {
+	$start_loop_microtime = microtime(true);
+	if ($seconds_of_day > time() - strtotime('today')) {
+		if ($sent_messages < $user_set_array['mailer_limit']) {
 			$email = $user_set_array['email_address'];
 			$header  = 'MIME-Version: 1.0' . PHP_EOL;
 			$header .= 'Content-type: text/plain; charset=iso-8859-1' . PHP_EOL;
@@ -32,24 +32,27 @@ while (true) {
 			$header .= 'Reply-To: ' . $user_set_array['email_address'] . PHP_EOL;
 			$header .= 'X-Mailer: PHP ' . phpversion();
 			$body = '*** ' . count($hex_array) . ' Aircrafts Yesterday UTC ***' . PHP_EOL . PHP_EOL;
-                foreach ($hex_array as $key => $value) {
-                $body .= $key .' = ' . $value . ', ';
-            }
+			foreach ($hex_array as $key => $value) {
+				$body .= $key .' = ' . $value . ', ';
+			}
 			mail($user_set_array['email_address'], 'Your Daily Aircraft Stats', $body, $header);
-	    }
-        $sent_messages++;
-        $hex_array = array();
-    }
+		}
+		$sent_messages++;
+		$hex_array = array();
+	}
 
-    $x = 0;
-    $json_data_array = json_decode(file_get_contents($user_set_array['url_json']),true);
+	$x = 0;
+	$json_data_array = json_decode(file_get_contents($user_set_array['url_json']),true);
 
 	// loop through aircraft.json file
 	foreach ($json_data_array['aircraft'] as $row) {
 		isset($row['hex']) ? $ac_hex = $row['hex'] : $ac_hex = '';
-        if ($ac_hex != '') { @$hex_array[$ac_hex] ++; $last_run = time() - strtotime('today'); }
-    }
-    $seconds_of_day = time() - strtotime('today');
+		if ($ac_hex != '' and strpos($ac_hex, '~') > -1 and $ac_hex != '000000') {
+			@$hex_array[$ac_hex] ++;
+			$last_run = time() - strtotime('today');
+		}
+	}
+	$seconds_of_day = time() - strtotime('today');
 
 // generate terminal output and set sleep timer to get minimum a full second until next aircraft.json is ready to get fetched
 $runtime = (time() - $start_time);
